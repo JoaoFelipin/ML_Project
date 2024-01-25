2f7b4e6d39347134c7a4e9ff5372d08807e7e313
JoaoFelipin <joaoed.felipin@gmail.com>
2024-01-25 15:31:33 -0300

    wip

--- streamlit/app_streamlit.py
+++ streamlit/app_streamlit.py
@@ -8,19 +8,35 @@ st.title('Diabetes Prediction')
 
 #parametros
 idade = st.number_input(label='High BP',value=18,min_value=18,max_value=120)
-bmi = st.number_input(label='HighChol',value=30.0)
-children = st.slider(label='CholCheck',min_value=0)
-smoker = st.selectbox(label='BMI',options=[0,1])
+high_col = st.number_input(label='HighChol',value=30.0)
+CholCheck = st.slider(label='CholCheck',min_value=0)
+bmi = st.slider(label='BMI',min_value=0)
 smoker = st.selectbox(label='Smoker',options=[0,1])
+smoker = st.selectbox(label='Stroke',options=[0,1])
+smoker = st.selectbox(label='Heart Disease',options=[0,1])
+smoker = st.selectbox(label='Physical Activity',options=[0,1])
+smoker = st.selectbox(label='Fruits',options=[0,1])
+smoker = st.selectbox(label='Veggies',options=[0,1])
+smoker = st.selectbox(label='HvyAlcohol',options=[0,1])
+smoker = st.selectbox(label='HealthCare',options=[0,1])
+smoker = st.selectbox(label='NoDocBcCost',options=[0,1])
+smoker = st.selectbox(label='GenHealth',options=[0,1])
+smoker = st.selectbox(label='MentHealth',options=[0,1])
+smoker = st.selectbox(label='PhysHealth',options=[0,1])
+smoker = st.selectbox(label='DiffWalk',options=[0,1])
+smoker = st.selectbox(label='sex',options=[0,1])
+smoker = st.selectbox(label='Age',options=[0,1])
+smoker = st.selectbox(label='Education',options=[0,1])
+smoker = st.selectbox(label='Income',options=[0,1])
 
-with open('models/model.pkl','rb') as model_file:
+
+with open('trained_classifier.pkl','rb') as model_file:
     model = pickle.load(model_file)
 
 def Prediction():
     df_input = pd.DataFrame([{
         'age':idade,
         'bmi':bmi,
-        'children':children,
         'smoker':smoker
 
     }])
